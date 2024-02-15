d34e76821520edcf8ab5893f0b3f56877c63f877
abvee <icaliberdev@gmail.com>
2024-02-15 14:24:38 +0530

    Fixed negative slope x axis code

--- main.c
+++ main.c
@@ -47,7 +47,8 @@ int atoi(char *s) {
 
 // draw a graph with -ve slope
 void graph_neg(int m, int b) {
-	int y = b;
+	int y = b; // the y coordinate of the point
+	int yr; // the actual y coordinate
 	int x = 0; // how many spaces to the right we are of y axis
 	int max = (b > 0)?-b/2:2 * b; // Till what y value should we go ?
 	bool xaxis = false; // Have we drawn an xaxis ?
@@ -55,29 +56,35 @@ void graph_neg(int m, int b) {
 	printf("^\n|\n"); // y axis arrow head
 
 	// start at y intercept, go till y = 0 (recall m is -ve)
-
+	// TODO: possible optimisation
 	while (y >= max) {
-		for (int j = x - 1; j > 0; j--)
-			printf(" ");
+		yr = y; // Our real y coordinate is y
+
+		if (y != 0)
+			for (int j = x - 1; j > 0; j--)
+				printf(" ");
 
 		printf(". (%d, %d)", x, y);
 		x++;
-		y+=m;
+		y += m;
 
 		// Broken x axis code
 		if (xaxis == false && y <= 0) {
+			int o = x - 1;
 
-			for (int i = y - m; i > 0; i--)
+			for (; yr > 0; yr--)
 				printf("\n|");
 
-			for (int i = 0; i < 12; i++)
+			while (o-- > 0)
 				printf("-");
 
 			xaxis = true;
 		}
 
 		// y decreases by slope amount for x += 1. so print (m) \n
-		for (int j = m; j < 0; j++)
+		//printf("yr: %d y: %d\n", yr, y);
+
+		for (int j = yr; j > y; j--)
 			printf("\n|");
 	}
 }
